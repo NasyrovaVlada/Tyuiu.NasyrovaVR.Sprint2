6f6643bcc7731711832c0eaabeeeecfe14b26ee2
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-10-03 19:38:16 +0500

    Add project files.

---
+++ Tyuiu.NasyrovaVR.Sprint2.Task0.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint2.Task0.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint2.Task0.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9aef43e9-7c33-41fc-8fb0-3b51f21a458f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
