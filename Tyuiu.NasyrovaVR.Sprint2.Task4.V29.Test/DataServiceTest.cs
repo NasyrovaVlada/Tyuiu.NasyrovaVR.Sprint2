62f70bf03ba0ac8e658142e3b100d39a968b3ea4
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-10-07 19:24:09 +0500

    Task4

---
+++ Tyuiu.NasyrovaVR.Sprint2.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NasyrovaVR.Sprint2.Task4.V29.Lib;
+
+namespace Tyuiu.NasyrovaVR.Sprint2.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 20.1;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 8;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NasyrovaVR.Sprint2.Task4.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint2.Task4.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint2.Task4.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75e6c22a-73f2-4aa7-80c9-28f611c97230")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
