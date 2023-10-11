66fb615f556733d7a07b95fc9e35986768925ce5
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-10-11 23:05:52 +0500

    Task7

---
+++ Tyuiu.NasyrovaVR.Sprint2.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.NasyrovaVR.Sprint2.Task7.V11.Lib
+{
+    public class DataService : ISprint2Task7V11
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x==y) || ((x<=y) && (y >= 0) && (y <= 1)) || ((x <= 0) && (x >= -1) && (y >= 0) && (y <= 1)) || ((y <= 0) && (y >= -1)) && ((x <= 0) && (x >= -1))))
+            {
+                res = true;
+            }
+
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.NasyrovaVR.Sprint2.Task7.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ed3bb27-66c4-44bb-bc37-4e9492277266")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
