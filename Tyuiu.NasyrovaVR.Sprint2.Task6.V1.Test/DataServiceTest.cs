862907344e4b55cdabfa0d18c0d263071cdada76
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-10-12 21:10:27 +0500

    Task6

--- Tyuiu.NasyrovaVR.Sprint2.Task6.V1.Test/DataServiceTest.cs
+++ Tyuiu.NasyrovaVR.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -13,18 +13,18 @@ namespace Tyuiu.NasyrovaVR.Sprint2.Task6.V1.Test
         {
             DataService ds = new DataService();
 
-            Assert.AreEqual("31", ds.FindMonthDaysCount(1));
-            Assert.AreEqual("28", ds.FindMonthDaysCount(2));
-            Assert.AreEqual("31", ds.FindMonthDaysCount(3));
-            Assert.AreEqual("30", ds.FindMonthDaysCount(4));
-            Assert.AreEqual("31", ds.FindMonthDaysCount(5));
-            Assert.AreEqual("30", ds.FindMonthDaysCount(6));
-            Assert.AreEqual("31", ds.FindMonthDaysCount(7));
-            Assert.AreEqual("31", ds.FindMonthDaysCount(8));
-            Assert.AreEqual("30", ds.FindMonthDaysCount(9));
-            Assert.AreEqual("31", ds.FindMonthDaysCount(10));
-            Assert.AreEqual("30", ds.FindMonthDaysCount(11));
-            Assert.AreEqual("31", ds.FindMonthDaysCount(12));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(1));
+            Assert.AreEqual(28, ds.FindMonthDaysCount(2));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(3));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(4));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(5));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(6));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(7));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(8));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(9));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(10));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(11));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(12));
 
             Assert.ThrowsException<ArgumentException>(() =>
             {
